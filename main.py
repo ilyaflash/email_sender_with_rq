e851ac84ee16115e8ccb176b2fd2e592a296a53b
ilyaflash <47789524+ilyaflash@users.noreply.github.com>
2019-02-25 11:14:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,27 +3,24 @@
 
 from rq import Queue
 from redis import Redis
-from json import dump, loads
+from json import loads
 from task import send_mail_from_app4hiiigmailcom
 
 #Save confs fun
+#from json import dump
 #def confs_to_json(path='mails.json'):
 #    confs = [{'to_addrs': ['ilyaflash@list.ru'], 'msg': 'Моё тестовое сообщение 1', 'subject': 'Моя тема 1'},
 #             {'to_addrs': ['ilyaflash@list.ru'], 'msg': 'Моё тестовое сообщение 2', 'subject': 'Моя тема 2'},
 #             {'to_addrs': ['ilyaflash@list.ru'], 'msg': 'Моё тестовое сообщение 3', 'subject': 'Моя тема 3'}]
 #    with open(path, 'w') as file:
 #        dump(confs, file, ensure_ascii=False)
+#confs_to_json()
 
 #Load confs fun
 def get_json_confs(path='mails.json'):
     with open(path, 'r') as file:
         return loads(file.read(), encoding='utf-8')
-    
-#Load mails
-confs = get_json_confs(path='mails.json')
 
-#create que
-queue = Queue(name='qname', connection=Redis(), is_async=True)
-
-#to que
-jlist = [queue.enqueue(send_mail_from_app4hiiigmailcom, i) for i in confs]
+queue = Queue(name='qname', connection=Redis(), is_async=True)                     #create que
+confs = get_json_confs(path='mails.json')                                          #load confs
+jlist = [queue.enqueue(send_mail_from_app4hiiigmailcom, i) for i in confs]         #to que
--- worker.py
+++ worker.py
@@ -2,10 +2,8 @@
 
 import os, redis
 from rq import Worker, Queue, Connection
-
-listen = ['qname']
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 if __name__ == '__main__':
     with Connection(conn):
-        worker = Worker(map(Queue, listen)).work()
+        worker = Worker(map(Queue, ['qname'])).work()
